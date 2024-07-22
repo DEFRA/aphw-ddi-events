755744add079f55158b6a48af18058be7d1cb7da
work <jeremy.barnsley@esynergy.co.uk>
2024-07-22 16:07:03 +0100

    Added test

--- test/unit/inbound/process-event.test.js
+++ test/unit/inbound/process-event.test.js
@@ -2,6 +2,9 @@ jest.mock('../../../app/inbound/handle-event/handle-event')
 const { handleEvent } = require('../../../app/inbound/handle-event/handle-event')
 const { processEvent } = require('../../../app/inbound')
 
+jest.mock('../../../app/messaging/send-alert')
+const { sendAlert } = require('../../../app/messaging/send-alert')
+
 describe('process event', () => {
   test('should process a permanently deleted event', async () => {
     const expectedEventType = 'event.delete.permanent'
@@ -14,5 +17,20 @@ describe('process event', () => {
     await processEvent(event)
 
     expect(handleEvent).toHaveBeenCalledWith(event, expectedEventType)
+    expect(sendAlert).not.toHaveBeenCalled()
+  })
+
+  test('should process a warning event', async () => {
+    const expectedEventType = 'warning'
+    const event = {
+      partitionKey: 'ED300038',
+      type: 'uk.gov.defra.ddi.warning.dummy',
+      message: '{"actioningUser":{"username":"overnight-job-system-user","displayname":"Overnight Job System User"},"operation":"warning","deleted":{"indexNumber":"ED300038"}}'
+    }
+
+    await processEvent(event)
+
+    expect(handleEvent).toHaveBeenCalledWith(event, expectedEventType)
+    expect(sendAlert).toHaveBeenCalledWith(event)
   })
 })
