235d8fd4a21e397aed437e555d5f3ec83cda57d2
Chris Cole <whitewaterdesign>
2024-10-17 13:25:52 +0100

    feat: 459655 - Add date helpers

---
+++ app/lib/date-helpers.js
@@ -0,0 +1,38 @@
+const { format } = require('date-fns')
+
+const formatDate = (date) => {
+  return format(date, 'dd/MM/yyyy')
+}
+
+const formatDateAsUTCNoTime = (date) => {
+  return format(date, 'yyyy-MM-dd')
+}
+
+const addYears = (date, years) => {
+  const newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+  newDate.setFullYear(newDate.getFullYear() + years)
+  return newDate
+}
+
+const addMinutes = (date, minutes) => {
+  const newDate = new Date(date)
+  newDate.setTime(newDate.getTime() + (minutes * 60 * 1000))
+  return newDate
+}
+
+const dateTodayOrInFuture = (date) => {
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  const checkedDate = new Date(date)
+  checkedDate.setHours(0, 0, 0, 0)
+  return checkedDate.getTime() >= today.getTime()
+}
+
+module.exports = {
+  formatDate,
+  formatDateAsUTCNoTime,
+  addYears,
+  addMinutes,
+  dateTodayOrInFuture
+}
